b40507caa25d9a9a008c313627bad0d257fc0d53
Kevin Lau <laukevin@us.ibm.com>
2018-02-01 14:25:53 -0600

    Separate build script.

---
+++ buildTestWebsite.sh
@@ -0,0 +1,24 @@
+if [ ! -z $GITHUB_TRAVIS_TOKEN ]; then
+    echo "Start requesting a travis build from upstream repository...";
+    # Doesn't use --debug on `travis login`.  --debug will show the github token
+    # travis login --skip-completion-check --org --github-token "$GITHUB_TRAVIS_TOKEN";
+    export TRAVIS_ACCESS_TOKEN=`travis login --skip-completion-check --org --github-token "$GITHUB_TRAVIS_TOKEN" && travis token`;
+    UPSTREAM_REPO_SLUG="kevinwlau%2Fopenliberty.io"
+    echo $TRAVIS_COMMIT_MESSAGE
+    body='{
+        "request": {
+            "message": "Build request from static guide",
+            "branch":"test"
+        }}'
+    curl -s -S -f -X POST \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -H "Travis-API-Version: 3" \
+        -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
+        -d "$body" \
+        https://api.travis-ci.org/repo/$UPSTREAM_REPO_SLUG/requests
+        
+    echo "SUCCESS: Completed request for travis build from upstream repository...";
+else
+    echo "ERROR: Missing Github token.  Cannot request a travis build for upstream repository...";
+fi
